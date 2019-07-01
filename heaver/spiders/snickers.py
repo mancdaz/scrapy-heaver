87d2e7c350f0efbe4e660faec3e5b00426fee118
Darren Birkett <darren.birkett@gmail.com>
2019-07-01 17:47:21 +0100

    better method for extracting colours/names

--- heaver/spiders/snickers.py
+++ heaver/spiders/snickers.py
@@ -3,6 +3,8 @@
 import scrapy
 import re
 
+cols = {}
+
 class QuotesSpider(scrapy.Spider):
     name = "snickers"
 
@@ -10,33 +12,39 @@ class QuotesSpider(scrapy.Spider):
         'http://www.snickersworkwear.co.uk/workwear/?items-per-page=all',
     ]
 
+
     def parse(self, response):
+
+        global cols
 
         # get all colors and convert to names and pop in a dict for lookup
         # later
-        colours = list(set(response.css('.colors img::attr(title)').getall()))
-        cols = {}
+        colours = list(set(response.css('.colors a img::attr(src)').getall()))
         for colour in colours:
-            if colour:
-                m = re.match(r"([0-9]*)(.+)", colour)
-                col_num = m.groups()[0]
-                col_desc = m.groups()[1]
-                cols[col_num] = col_desc
-        #print(cols)
+            colour_num = colour.rpartition('/')[2].rsplit('.')[0]
+            colour_desc = response.css('.colors a img[src="%s"]::attr(alt)' % colour).get()
+            cols[colour_num] = colour_desc
 
         for href in response.css('.product .colors a::attr(href)').extract():
             url = response.urljoin(href)
             req = scrapy.Request(url, callback = self.parse_product_page)
-            req.meta['item_link'] = url
-            req.meta['cols'] = cols
             yield req
 
 
     def parse_product_page(self, response):
-        #print(response.meta.get('cols'))
+
+        global cols
+
+        item_link = response.url
+        print('item link is %s: ' % item_link)
+        item_colour_num = item_link.rsplit('=')[1]
+        item_colour_desc = cols[item_colour_num]
+
         yield {
-            'item_link' : response.meta.get('item_link'),
             'item_name' : response.css('h1::text').extract(),
+            'item_color_num': item_colour_num,
+            'item_colour_desc': item_colour_desc,
+            'item_link' : item_link,
             'item_desc' : response.css('p.intro::text').extract(),
             'item_features' : response.css('.p-c ul li::text').extract(),
             #'item_sizes' : ,
